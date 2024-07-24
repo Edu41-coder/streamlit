9e6a4fd637fc5d6803f695e84a1882595c3d8f52
Edu41-coder <153880703+Edu41-coder@users.noreply.github.com>
2024-07-24 12:25:02 +0200

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -166,26 +166,20 @@ L'objectif principal est de démontrer comment préparer des séries temporelles
         0.06091821,  0.93024063]), 'actual': array([ 0.78221838, -0.89878661,  1.78092046, -0.72888976, -0.49897477,
         0.90756602,  2.02660878, -0.12930764,  0.16590115,  0.02335511,
        -0.1397481 ,  1.32072761])}""")
-        st.markdown("""Interprétation des Résultats
-1. Nom du Modèle: Random Forest Regressor
-Le modèle utilisé est un Random Forest Regressor, qui est un ensemble d'arbres de décision utilisé pour la régression.
-RMSE (Root Mean Squared Error): 0.9766962889421072
-Le RMSE mesure l'écart type des résidus (erreurs de prédiction). Une valeur plus faible indique un meilleur ajustement du modèle aux données. Ici, un RMSE de 0.98 est relativement faible, ce qui suggère que le modèle a une bonne précision.
-3. MAPE (Mean Absolute Percentage Error): 2.395501224701597
-Le MAPE mesure l'erreur moyenne en pourcentage entre les valeurs prédites et les valeurs réelles. Un MAPE de 2.40% indique que, en moyenne, les prédictions du modèle sont à 2.40% près des valeurs réelles.
-MASE (Mean Absolute Scaled Error): 0.4817534131809827
+        st.markdown("""Analyse des Métriques de Performance
+1. RMSE (Root Mean Squared Error) :
+Valeur : 0.9896255226157039
+Le RMSE mesure l'écart type des résidus (erreurs de prédiction). Une valeur plus faible indique un meilleur ajustement du modèle aux données. Ici, un RMSE de 0.99 est relativement faible, ce qui suggère que le modèle a une bonne précision.
+2. MAPE (Mean Absolute Percentage Error) :
+Valeur : 1.6330739234680127
+Le MAPE mesure l'erreur moyenne en pourcentage entre les valeurs prédites et les valeurs réelles. Un MAPE de 1.63% indique que, en moyenne, les prédictions du modèle sont à 1.63% près des valeurs réelles.
+MASE (Mean Absolute Scaled Error) :
+Valeur : 0.4701224490257277
 Le MASE est une mesure de l'erreur de prévision qui est indépendante de l'échelle des données. Une valeur de MASE inférieure à 1 indique que le modèle est meilleur que la moyenne des erreurs absolues d'un modèle de référence simple (comme la moyenne historique).
-Prédictions (yhat):
-Les valeurs prédites par le modèle pour les données de test sont : [ 0.58284685, -0.40872554, 0.65940835, -0.84601683, 0.32481906, 0.42258725, -0.70298003, -0.6172057 , -0.36839461, -0.2939558 , -0.02339315, 0.45449389].
-6. Résidus (resid):
-Les résidus (différence entre les valeurs réelles et les valeurs prédites) sont : [ 0.20318174, -0.50080062, 1.13396647, 0.10785805, -0.83107285, 0.48987391, 2.7441697 , 0.48381865, 0.5327715 , 0.31455286, -0.12052472, 0.87470494].
-7. Valeurs Réelles (actual):
-Les valeurs réelles des données de test sont : [ 0.78602859, -0.90952616, 1.79337482, -0.73815878, -0.50625379, 0.91246116, 2.04118968, -0.13338705, 0.16437689, 0.02059706, -0.14391787, 1.32919883].
-Conclusion
-Le modèle Random Forest Regressor montre une bonne performance avec un RMSE relativement faible et un MAPE de 2.40%, ce qui indique que les prédictions sont assez précises. Le MASE inférieur à 1 confirme que le modèle est performant par rapport à un modèle de référence simple. Les résidus montrent les écarts entre les valeurs réelles et prédites, et bien que certains écarts soient notables, la performance globale reste satisfaisante.
-
-
-""")
+ """)
+        st.markdown(""" Conclusion
+Le modèle Random Forest Regressor montre une bonne performance avec un RMSE relativement faible et un MAPE de 1.63%, ce qui indique que les prédictions sont assez précises. Le MASE inférieur à 1 confirme que le modèle est performant par rapport à un modèle de référence simple. Les résidus montrent les écarts entre les valeurs réelles et prédites, et bien que certains écarts soient notables, la performance globale reste satisfaisante.
+Ces résultats suggèrent que le modèle est capable de capturer les tendances et les variations des séries temporelles des décès (serie_morts) de manière efficace.""")
         st.markdown(f"Résultats serie accidents")
         st.markdown("""Results for accs:
 {'Model Name': 'Random Forest Regressor', 'Model': RandomForestRegressor(), 'RMSE': 0.7360697791883707, 'MAPE': 1.929022923086171, 'MASE': 0.4242266277968524, 'yhat': array([-0.32188171,  0.34778839, -0.00321162,  0.57383633, -0.86493909,
@@ -195,8 +189,19 @@ Le modèle Random Forest Regressor montre une bonne performance avec un RMSE rel
        -0.39342825, -0.04339357]), 'actual': array([ 0.29953261,  0.2666904 ,  0.04819536,  0.70271017, -0.05981246,
        -0.28282967,  1.3853749 ,  1.32465056,  0.67089546,  0.41688225,
        -0.9674606 ,  0.62449082])}""")
-        st.markdown("""
-Le modèle Random Forest Regressor montre une bonne performance avec un RMSE relativement faible et un MAPE de 2.78%, ce qui indique que les prédictions sont assez précises. Le MASE inférieur à 1 confirme que le modèle est performant par rapport à un modèle de référence simple. Les résidus montrent les écarts entre les valeurs réelles et prédites, et bien que certains écarts soient notables, la performance globale reste satisfaisante.""")
+        st.markdown("""Analyse des Métriques de Performance
+RMSE (Root Mean Squared Error) :
+Valeur : 0.7360697791883707
+Le RMSE mesure l'écart type des résidus (erreurs de prédiction). Une valeur plus faible indique un meilleur ajustement du modèle aux données. Ici, un RMSE de 0.74 est relativement faible, ce qui suggère que le modèle a une bonne précision.
+MAPE (Mean Absolute Percentage Error) :
+Valeur : 1.929022923086171
+Le MAPE mesure l'erreur moyenne en pourcentage entre les valeurs prédites et les valeurs réelles. Un MAPE de 1.93% indique que, en moyenne, les prédictions du modèle sont à 1.93% près des valeurs réelles.
+3. MASE (Mean Absolute Scaled Error) :
+Valeur : 0.4242266277968524
+Le MASE est une mesure de l'erreur de prévision qui est indépendante de l'échelle des données. Une valeur de MASE inférieure à 1 indique que le modèle est meilleur que la moyenne des erreurs absolues d'un modèle de référence simple (comme la moyenne historique). """)
+        st.markdown(""Conclusion
+Le modèle Random Forest Regressor montre une bonne performance avec un RMSE relativement faible et un MAPE de 1.93%, ce qui indique que les prédictions sont assez précises. Le MASE inférieur à 1 confirme que le modèle est performant par rapport à un modèle de référence simple. Les résidus montrent les écarts entre les valeurs réelles et prédites, et bien que certains écarts soient notables, la performance globale reste satisfaisante.
+Ces résultats suggèrent que le modèle est capable de capturer les tendances et les variations des séries temporelles des accidents (serie_accs) de manière efficace. """)
         st.image('intro/importance_accidents.png')
         st.markdown("""Conclusion
 L'analyse des importances des variables montre que les valeurs de la série temporelle 12 mois avant (x_12) et 1 mois avant (x_1) la période actuelle sont les plus critiques pour les prédictions du modèle RandomForestRegressor sur les données de décès. Cela suggère une forte saisonnalité annuelle ainsi qu'une dépendance à court terme dans les données. Les autres variables, bien qu'importantes, ont un impact moindre sur les prédictions.""")
