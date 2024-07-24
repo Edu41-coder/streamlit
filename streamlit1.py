64371fa3d49d2f3a506acf82c83d22b3e25190b0
Edu41-coder <153880703+Edu41-coder@users.noreply.github.com>
2024-07-24 11:48:26 +0200

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -151,6 +151,7 @@ L'objectif principal est de démontrer comment préparer des séries temporelles
         # Ajout des images pour le modèle Random Forest
         st.image('intro/display_ser_morts.PNG')
         st.image('intro/display_serie_accs.PNG')
+        st.markdown(f"\n\n")
         st.markdown(f"Serie morts")
         st.image('intro/serie_morts.PNG')
         st.image('intro/serie_morts_log.png')
